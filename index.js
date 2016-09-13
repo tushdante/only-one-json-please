c9fbb123453a8aebbf7a85a04602f197435e8a5a
Matt Hamil <j.matthew.hamil@gmail.com>
2016-09-12 19:59:35 -0500

    Initial files

---
+++ index.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+'use strict';
+const fs = require('fs');
+const request = require('request');
+const jsonfile = require('jsonfile');
+const glob = require('glob');
+
+module.exports = function() {
+  glob(`${__dirname}/*.json`, function (err, files) {
+    if (err) {
+      console.log(`ERROR: Unable to read the folder: ${__dirname}`, err);
+    }
+
+    const masterObj = {};
+
+    files.forEach(function(file, index) {
+      fs.readFile(file, 'utf8', function(err, data) {
+        if (err) throw err;
+
+        let obj = JSON.parse(data);
+
+        Object.keys(obj).forEach((key) => {
+          masterObj[key] = obj[key];
+          jsonfile.writeFileSync(`./output.json`, masterObj);
+        });
+      });
+    });
+  });
+};
