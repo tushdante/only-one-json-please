7e700ca6a6a97779d8c780e487c2bf90f2d204de
Matt Hamil <j.matthew.hamil@gmail.com>
2016-09-12 20:24:26 -0500

    Package now working.

--- index.js
+++ index.js
@@ -6,25 +6,26 @@ const request = require('request');
 const jsonfile = require('jsonfile');
 const glob = require('glob');
 
-shell.exec('echo "Concatenating files in the current directory..."');
+shell.exec('echo "Concatenating files in $PWD"');
 
-  glob(`${__dirname}/*.json`, function (err, files) {
-    if (err) {
-      console.log(`ERROR: Unable to read the folder: ${__dirname}`, err);
-    }
+glob(`${process.cwd()}/*.json`, function (err, files) {
+  if (err) {
+    console.log(`ERROR: Unable to read the folder: ${process.cwd()}`, err);
+  }
 
-    const masterObj = {};
+  const masterObj = {};
 
-    files.forEach(function(file, index) {
-      fs.readFile(file, 'utf8', function(err, data) {
-        if (err) throw err;
+  files.forEach(function(file, index) {
+    fs.readFile(file, 'utf8', function(err, data) {
+      if (err) throw err;
 
-        let obj = JSON.parse(data);
+      let obj = JSON.parse(data);
 
-        Object.keys(obj).forEach((key) => {
-          masterObj[key] = obj[key];
-          jsonfile.writeFileSync(`./output.json`, masterObj);
-        });
+      Object.keys(obj).forEach((key) => {
+        masterObj[key] = obj[key];
+        jsonfile.writeFileSync(`${process.cwd()}/output.json`, masterObj);
       });
     });
   });
+  console.log('Done: output.json');
+});
